a6ed83190cd5ecc55f298e1c6f48b8fb367c1570
Daniel <danielbamidele92@gmail.com>
2021-10-01 08:00:38 +0100

    Sassy Ticketing system complete

--- Ticketing system.py
+++ Ticketing system.py
@@ -1,19 +1,28 @@
 # Roller Coaster Ticketing system
 print("Hello! Hello! Hello!"
-      "\nWelcome to my very Daniel's Ticketing system😁\n"
+      "\nWelcome to Dee's Ticketing system😁\n"
       "The goal is pretty much simple\n"
-      "You give me your age and height, Then i'll tell you if you can ride or not/how much you need to pay😇")
+      "You input your age and height, Then i'll tell you if you can ride or not/how much you need to pay😇")
 
 height = int(input("What is your Height in cm: "))
-age = int(input("What if your age?: "))
+age = int(input("What is your Age?: "))
 
-if height >= 120 and age > 5:
+if height >= 150 and age > 5:
     if age > 18:
-        print("You can Hop on for the best ride of your life. Your Ticket will cost £12, Thank you!🥰")
+        bill = 12
+        print(f"You can Hop on for the best ride of your life. Adult Tickets are £{bill}, Thank you!🥰")
     elif age <= 18 and age >= 12:
-        print("You can Hop on for the best ride of your life. Your Ticket will cost £7, Thank you!🥰")
+        bill = 7
+        print(f"You can Hop on for the best ride of your life. Youth tickets are £{bill}, Thank you!🥰")
     else:
-        print("You can Hop on for the best ride of your life. Your Ticket will cost £5, Thank you!🥰")
+        bill = 5
+        print(f"You can Hop on for the best ride of your life. Your Ticket will cost £{bill}, Thank you!🥰")
+    wants_photo = input("Do you want a photo taken? Y or N. ")
+    if wants_photo == "Y":
+        bill += 3
+        print(f"Your final bill is £{bill}")
+    else:
+        print(f"Oh well, Final ticket price is £{bill}")
 else:
     print("I'm so sorry, I can't let you ride until you grow taller/older😭\n"
           "My advice is that you eat more beans and come back in a few years😏\n"
